e265720b67a58197999118aee1f530ca9782c523
Angeles, Michael (US) <michael.angeles@lmco.com>
2023-11-14 03:33:08 +0000

    More

--- config.sh
+++ config.sh
@@ -1,2 +1,4 @@
+echo "Yeet"
+
 export PATH="/opt/drake/bin${PATH:+:${PATH}}"
 export PYTHONPATH="/opt/drake/lib/python$(python3 -c 'import sys; print("{0}.{1}".format(*sys.version_info))')/site-packages${PYTHONPATH:+:${PYTHONPATH}}"
